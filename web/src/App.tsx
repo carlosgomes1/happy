5381baed69b113787f6ba59ed92c9080ea5376a7
carlosgomes1 <48812516+carlosgomes1@users.noreply.github.com>
2020-10-15 02:17:14 -0300

    ok landing page structure with stylizations

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
 
 import './styles/global.css';
+import './styles/pages/landing.css';
+
+import logoImg from './images/logo.svg'
 
 function App() {
   return (
-    <h1> Happy </h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy"/>
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p> Visite orfanatos e mude o dia
+            de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>São Paulo</strong>
+          <span>São Paulo</span>
+        </div>
+
+        <a href="" className="enter-app">></a>
+      </div>
+    </div>
   );
 }
 
